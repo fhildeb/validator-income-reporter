46b7a208f0a385a1d5e458b333712366156d57e1
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2024-08-03 21:53:42 +0200

    feat: add config file

---
+++ config-sample.py
@@ -0,0 +1,18 @@
+BLOCKSCOUT_API_URL = 'https://explorer.execution.mainnet.lukso.network/api'
+
+COINMARKETCAP_API_URL = 'https://pro-api.coinmarketcap.com'
+COINMARKETCAP_API_KEY = '1a1a1a1a-1234-1234-1a1a-1a1a1a1a1a1a'
+COINMARKETCAP_FIAT_ID = '2790' # EUR = '2790', USD = '2781'
+COINMARKETCAP_CRYPTO_ID = '5625' # LYXe = '5625', LYX = '27622'
+COINMARKETCAP_HEADERS = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': COINMARKETCAP_API_KEY,
+}
+
+FIAT_CURRENCY = 'EUR'
+COIN_NAME = 'LYX'
+REPORT_TITLE = 'LUKSO Blockchain Node Validator'
+EXPLORER_LINK = 'https://explorer.execution.mainnet.lukso.network'
+
+ETH1_ADDRESS = '0xcafecafecafecafecafecafecafecafecafecafe'
+YEAR = 2023
