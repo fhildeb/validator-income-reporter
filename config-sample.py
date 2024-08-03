0a1ad3ce4013c84050d4a40703c3eaed9d3f87bd
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2024-08-03 21:54:27 +0200

    docs: add config documentation

--- config-sample.py
+++ config-sample.py
@@ -1,5 +1,41 @@
+""" 
+BLOCKSCOUT_API_URL is used to retrieve historical balance 
+data from the Blockscout explorer of a given blockchain.
+
+- LUKSO Mainnet Explorer is used as default
+- always use without version tag or slash at the end
+- always use HTTPS for data transport protocol
+"""
 BLOCKSCOUT_API_URL = 'https://explorer.execution.mainnet.lukso.network/api'
 
+"""
+COINMARKETCAP_API_URL, COINMARKETCAP_API_KEY, COINMARKETCAP_HEADERS,
+COINMARKETCAP_FIAT_ID, and COINMARKETCAP_CRYPTO_ID are used to retrieve 
+historical price information about a specific blockchain coin, matching
+the data cetrieved from Blockscout to calculate FIAT income.
+
+- always use API_URL without version tag or slash at the end
+- always use API_URL with HTTPS for data transport protocol
+
+- ensure that your API_KEY is valid
+- ensure that your API_KEY has enough credits left
+- credits used for a full yearly report: 9125
+
+- keep HEADER as is
+- ensure FIAT_ID is valid
+- ensure that CRYPTO_ID is valid and matches the blockchain
+
+- API KEY PRICING: https://coinmarketcap.com/api/pricing
+- FIAT ID CODES: - https://coinmarketcap.com/api/documentation/v1/#section/Standards-and-Conventions
+
+LUKSO Disclaimer: for LYX income reports for 2023, its recommended to use the 
+CRYPTO_ID of the LYXe token, instead of the LYX coin. This is because withdrawals 
+got enabled in June 2023, while LYX was listed on the first exchange end of July. 
+Due to a big liquidity pool being actively bridged from LYXe, both crypto assets 
+only had minimal price fluctuations. For 2024 and beyond, please always use LYX.
+- https://coinmarketcap.com/currencies/lukso/
+- https://coinmarketcap.com/currencies/lukso-network/
+"""
 COINMARKETCAP_API_URL = 'https://pro-api.coinmarketcap.com'
 COINMARKETCAP_API_KEY = '1a1a1a1a-1234-1234-1a1a-1a1a1a1a1a1a'
 COINMARKETCAP_FIAT_ID = '2790' # EUR = '2790', USD = '2781'
@@ -9,10 +45,23 @@ COINMARKETCAP_HEADERS = {
     'X-CMC_PRO_API_KEY': COINMARKETCAP_API_KEY,
 }
 
+"""
+FIAT_CURRENCY, COIN_NAME, REPORT_TITLE, and EXPLORER_LINK are used 
+for naming columns and values within the CSV and PDF reports, 
+as well as creating a cover sheet
+"""
 FIAT_CURRENCY = 'EUR'
 COIN_NAME = 'LYX'
 REPORT_TITLE = 'LUKSO Blockchain Node Validator'
 EXPLORER_LINK = 'https://explorer.execution.mainnet.lukso.network'
 
+"""
+ETH1_ADDRESS and YEAR are used to fetch blockchain data and
+calculate report timeframes.
+
+- ensure that the ETH1_ADDRESS is your validator withdrawal address
+- ensure that the blockchain was active during the given year
+- ensure that the year is a valid number
+"""
 ETH1_ADDRESS = '0xcafecafecafecafecafecafecafecafecafecafe'
 YEAR = 2023
